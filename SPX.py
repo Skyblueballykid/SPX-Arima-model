7cc97724b18795ef30696d2c0b3f5e3764eecaee
Thomas Kalnik <kalnik.thomas@gmail.com>
2018-05-02 21:41:41 -0400

    Add files via upload

---
+++ SPX.py
@@ -0,0 +1,17 @@
+import numpy as np
+import pandas as pd
+import statsmodels as stsmd
+import matplotlib as mplib
+
+
+SPX = pd.read_csv("SPX Weekly 2000-2017 (2).csv")
+
+
+print(SPX.head(n=5))
+
+model = ARIMA(SPX, order=(0,1,0))
+model_fit = model.fit()
+
+outcome = model_fit.forecast()[0]
+
+print(model.head(n=5))
